1208b3e38a3a28ec705d7860c269fd03ea718326
Ruslantarget <ruslan.bulatov.96@gmail.com>
2022-11-07 19:13:18 +0300

    methods

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ console.log('FIND:', myFind(array, myFindCallback));
 
 //! findIndex
 const myFindIndexCallback = (item) => {
-    return item === 13;
+    return item === -5;
 };
 
 let d = array.findIndex(myFindIndexCallback);
@@ -167,52 +167,44 @@ const myFindIndex = (arr, callbackFn) => {
         if (callbackFn(arr[index], index, arr)) {
             return index;
         }
-    }
+    } return -1;
 };
 console.log('FIND INDEX:', myFindIndex(array, myFindIndexCallback));
 
 //! includes
-const myIncludesCallback = (item) => {
-    return item > 12;
-};
+let e = array.includes(11, 5);
 
-let e = array.includes(myIncludesCallback);
-
-const myIncludes = (arr, callbackFn) => {
-    for (let index = 0; index < arr.length; index++) {
-        if (callbackFn(arr[index], index, arr)) {
+const myIncludes = (arr, num, startIndex = 0) => {
+    for (let index = startIndex; index < arr.length; index++) {
+        if (num === arr[index]) {
             return true;
         }
     } return false;
 };
-console.log('INCLUDES:', myIncludes(array, myIncludesCallback));
+console.log('INCLUDES:', myIncludes(array, 11, 5));
 
 //! indexOf ???
-const myIndexOfCallback = (item) => {
-    return item === 8;
-};
+let f = array.indexOf(11, 5);
 
-let f = array.includes(myIndexOfCallback);
-
-const myIndexOf = (arr, callbackFn) => {
-    for (let index = 0; index < arr.length; index++) {
-        if (callbackFn(arr[index], index, arr)) {
+const myIndexOf = (arr, num, startIndex = 0) => {
+    for (let index = startIndex; index < arr.length; index++) {
+        if (num === arr[index]) {
             return index;
         }
-    }
+    } return -1;
 };
-console.log('INDEX OF:', myIndexOf(array, myIndexOfCallback));
+console.log('INDEX OF:', myIndexOf(array, 11, 5));
 
 //! splice arr.splice (fromIndex, itemsToDelete, item1ToAdd, item2ToAdd, ...)
 const spliceArray = [3, 44, 6, 90, 18, 1, 2, 8, 9, 11, 13, 18, 41];
 
 let g = spliceArray.splice(2, 10, 'hello');
+console.log('g', g);
 
 const mySplice = (arr, start, remove, add) => {
     for (let index = start; index < start + remove; index++) {
         
     }
-    
 };
 console.log('SPLICE:', mySplice(spliceArray, 2, 10, 'hello'));
 
@@ -220,12 +212,17 @@ console.log('SPLICE:', mySplice(spliceArray, 2, 10, 'hello'));
 const sliceArray = [3, 44, 6, 90, 18, 1, 2, 8, 9, 11, 13, 18, 41];
 
 let slicedArray = sliceArray.slice(2, 7);
-console.log(slicedArray);
 
-//! reverse
+const mySlice = (arr, start = 0, end) => {
+    const newSlice = [];
+    end = end ?? arr.length;
+    for (let index = start; index < end; index++) {
 
+        newSlice.push(arr[index]);
 
-
+    } return newSlice;
+};
+console.log('SLICE:', mySlice(sliceArray, 2, 7));
 
 //! forEach (callback)
 const forEach = ["Яблоко", "Апельсин", "Груша"];
@@ -241,12 +238,7 @@ const myForEach = (arr, callbackFn) => {
         callbackFn(arr[index], index, arr);
     }
 };
-myForEach(forEach, myForEachCallback);
-
-//! map
-
-
-
+console.log('FOR EACH:', myForEach(forEach, myForEachCallback));
 
 //! filter
 const filterArray = [
@@ -278,18 +270,38 @@ const myFilterCallback = (item) => {
 };
 
 let newFilterArray = filterArray.filter(myFilterCallback);
-// console.log(newFilterArray);
-
-const newArray = [];
 
 const myFilter = (arr, callbackFn) => {
+    const newArray = [];
     for (let index = 0; index < arr.length; index++) {
-        callbackFn(arr[index], index, arr);
+        if (callbackFn(arr[index], index, arr)) {
+            newArray.push(arr[index]);
+        }
     }   return newArray;
 };
-console.log('FILTER:', myFilter(newArray, myFilterCallback));
+console.log('FILTER:', myFilter(filterArray, myFilterCallback));
+
+//! reverse
+
+
+
 
 //! reduce
 
 
+
+
+//! map
+
+const myMapCallback = (o) => {
+    return {...o, fullname: o.name + ' ' + o.lastname};
+};
+
+const myMap = (arr, callbackFn) => {
+    const newMapArr = [];
+    for (let index = 0; index < arr.length; index++) {
+        newMapArr.push(callbackFn(arr[index], index, arr));
+    } return newMapArr;
+};
+console.log('MAP:', myMap(filterArray, myMapCallback));
 
